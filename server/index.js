41615db0c9d91895c88290c981d9a762ffee47bd
Harshit <harshitpushkarnais@gmail.com>
2026-05-13 12:15:44 +0530

    feat(server): setup express server and base config

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes')
+const {errorMiddleware} = require('./middlewares/errorMiddleware')
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(morgan('dev'));
+
+// Routes Placeholder
+app.use('/api/v1/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('AI SaaS API is running...');
+});
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
