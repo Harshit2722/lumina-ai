5c0cb85cb20b3598d12b57810e4baaf0a77d6191
Harshit <harshitpushkarnais@gmail.com>
2026-05-15 01:19:15 +0530

    feat(server): add Cookie-Parser and update CORS configuration

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes')
-const {errorMiddleware} = require('./middlewares/errorMiddleware')
+const {errorMiddleware} = require('./middlewares/errorMiddleware');
+const cookieParser = require('cookie-parser');
 
 // Connect to MongoDB
 connectDB();
@@ -13,8 +14,13 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cookieParser())
+app.use(cors({ 
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true 
+}));
 app.use(morgan('dev'));
+
 
 // Routes Placeholder
 app.use('/api/v1/auth', authRoutes);
